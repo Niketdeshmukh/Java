48cdd5b96b1cfd4c1b9dc5039205a705009e03cb
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2022-08-10 13:21:17 +0530

    new files added

--- src/MirroredInvertedRightDownTriangle.java
+++ src/MirroredInvertedRightDownTriangle.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
-public class MirroredInvertedRightTriangleStar {
+public class MirroredInvertedRightDownTriangle {
     public static void main(String[] args) {
-        System.out.println("Enter the no : ");
+        System.out.println("Enter the value : ");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        for(int i=1;i<=num;i++)
-        {
-            for(int j=1;j<=num-i;j++)
-            {
+
+        for (int i = 1; i <=num; i++) {
+            for (int j = 1; j <=i ; j++) {
                 System.out.print(" ");
             }
-            for(int j=1;j<=i;j++)
-            {
+            for (int j = num; j >=i; j--) {
                 System.out.print("*");
             }
             System.out.println();
---
+++ src/TriangleStar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TriangleStar {
+    public static void main(String[] args) {
+        System.out.println("Enter the value : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 0; i <num; i++) {
+            for (int j = num-i; j >1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j<=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
