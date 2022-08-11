1b36ecb943f202aeacad4d8d255bbd4705e20177
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2022-08-11 22:27:38 +0530

    new files added

--- src/VHeightStar.java
+++ src/VHeightStar.java
@@ -4,15 +4,26 @@ public class VHeightStar {
     public static void main(String[] args) {
         System.out.println("Enter the value : ");
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        for (int i = 1; i <=num ; i++) {
-            for (int j = 1; j <=i ;j++) {
+        int n = sc.nextInt();
+        int i, j;
+        for (i = n - 1; i >= 0; i--)
+        {
+            // outer gap loop
+            for (j = n - 1; j > i; j--)
+            {
                 System.out.print(" ");
             }
-            for (int j = num-1; j>=i ; j--) {
-                System.out.print("* ");
-            }
-            System.out.println();
+
+            // 65 is ASCII of 'A'
+            System.out.print("*");
+
+            // inner gap loop
+            for (j = 1; j < (i * 2); j++)
+                System.out.print(" ");
+
+            if (i >= 1)
+                System.out.print("*");
+            System.out.print("\n");
         }
     }
 }
--- src/XPatternStar.java
+++ src/XPatternStar.java
@@ -5,24 +5,16 @@ public class XPatternStar {
         System.out.println("Enter the value : ");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        if(num%2==0){
-        for (int i = 0; i < num; i++) {
-            for (int j = 0; j < num; j++) {
-                if (i + j == num || i == j) {
+        int k = num*2-1;
+        for (int i = 1; i <= k; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (i==j || j==k-i+1){
                     System.out.print("*");
-                } else System.out.print(" ");
+                }
+                System.out.print(" ");
             }
             System.out.println();
-        }}
-        else {
-            for (int i = 1; i <=num; i++) {
-                for (int j = 1; j <= num; j++) {
-                    if (i + j == num || i == j) {
-                        System.out.print("* ");
-                    } else System.out.print(" ");
-                }
-                System.out.println();
-            }
         }
+
     }
 }
