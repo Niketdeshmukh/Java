0f81fc01f402552c1530798a56e9e4ee650c97ba
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2022-08-12 16:38:56 +0530

    new files added

---
+++ src/GreatestElementAndItsIndex.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class GreatestElementAndItsIndex {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the no of elements : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println("Enter the elements : ");
+        int[] array = new  int[num];
+
+
+    }
+}
