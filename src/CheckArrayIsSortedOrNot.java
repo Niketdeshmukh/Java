bb7521e8e1a42ac54de8b05dae136f541cb907f9
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2022-08-13 22:58:04 +0530

    new files added

---
+++ src/CheckArrayIsSortedOrNot.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CheckArrayIsSortedOrNot {
+    public static void main(String[] args) {
+        System.out.println("No of elements : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println("Enter the elements :");
+        int[] array = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i]=sc.nextInt();
+        }
+        boolean temp = true;
+        for (int i = 0; i < num-1; i++){
+            if (array[i]>array[i+1])
+           temp = false;
+
+        }
+        System.out.println(temp);
+    }
+}
