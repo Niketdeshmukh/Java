5d2feb6c7e3ffee93ef7b3a6436d643cebf2fa0b
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2022-08-14 23:21:59 +0530

    new files added

--- src/DiamondPatternStar.java
+++ src/DiamondPatternStar.java
@@ -6,7 +6,16 @@ public class DiamondPatternStar {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         for (int i = 0; i < num; i++) {
-            for (int j = 0; j <=i ; j++) {
+            for (int j = num-i; j >1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j<=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < num; i++){
+            for (int j = 0; j <i ; j++) {
                 System.out.print(" ");
             }
             for (int j = num-i; j>0 ; j--) {
