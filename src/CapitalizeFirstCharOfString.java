96416144674498ede712599891e856cfa1acba93
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2022-08-22 22:06:01 +0530

    new files added

--- src/CapitalizeFirstCharOfString.java
+++ src/CapitalizeFirstCharOfString.java
@@ -1,13 +1,13 @@
 public class CapitalizeFirstCharOfString {
     public static void main(String[] args) {
         String message = "everyone loves java";
-        char[] ch = message.toCharArray();
-        boolean foundSpace = true;
-        for(int i = 0; i < ch.length; i++) {
-            if(Character.isLetter(ch[i])) {
-                if(foundSpace) {
-                    ch[i] = Character.toUpperCase(ch[i]);
-                    foundSpace = false;
+        char ch[] = message.toCharArray();
+        boolean foundSpace=true;
+        for (int i = 0; i < ch.length; i++) {
+            if (Character.isLetter(ch[i])){
+                if (foundSpace){
+                    ch[i]=Character.toUpperCase(ch[i]);
+                    foundSpace=false;
                 }
             }
             else {
@@ -16,5 +16,5 @@ public class CapitalizeFirstCharOfString {
         }
         message = String.valueOf(ch);
         System.out.println("Message: " + message);
+        }
     }
-}
