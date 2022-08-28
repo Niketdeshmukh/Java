5fd8d0883cffd21f77788b7d5a7ff67011968c21
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2022-08-28 20:03:59 +0530

    new files added

--- src/MakeCapitalOfstartAndEndWord.java
+++ src/MakeCapitalOfstartAndEndWord.java
@@ -1,6 +1,19 @@
 public class MakeCapitalOfstartAndEndWord {
     public static void main(String[] args) {
         String str = "hi there welcome here";
+        char ch[]= str.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            int k=i;
+            while (i<ch.length && ch[i]!= ' '){
+                i++;
+            }
 
+            ch[k] = (char)(ch[k] >= 'a' && ch[k] <= 'z'
+                    ? ((int)ch[k] - 32)
+                    : (int)ch[k]);
+            ch[i-1]=(char)(ch[i-1]>='a' && ch[i-1]<='z'?((int)ch[i-1]-32):(int)ch[i-1]);
+        }
+        str = String.valueOf(ch);
+        System.out.print(str);
     }
 }
