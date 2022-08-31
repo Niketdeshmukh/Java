0320b9213778a3006f891190f4eb994102614aa0
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2022-08-31 17:32:58 +0530

    new files added

---
+++ src/SelfDividingNumberOrNot.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelfDividingNumberOrNot {
+    public static List<Integer> selfDividingNumbers(int left,int right){
+        List<Integer> list= new ArrayList<>();
+        for (int i = left; i <=right ; i++) {
+            if(selfDividing(i)){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+    public static boolean selfDividing(int i){
+        int num=i;
+        while(num>0){
+            int digit=num%10;
+            num = num /10;
+            if(digit==0|| i%digit>0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(selfDividingNumbers(1,22));
+
+    }
+}
