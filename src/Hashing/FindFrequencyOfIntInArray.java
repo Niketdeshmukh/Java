4c976256ad873e78699be7a5f02964efd2b7ed6b
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2022-09-05 19:56:25 +0530

    :octocat: :sparkles:

---
+++ src/Hashing/FindFrequencyOfIntInArray.java
@@ -0,0 +1,13 @@
+package Hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindFrequencyOfIntInArray {
+    public static void main(String[] args) {
+        int arr[] = {2,3,2,3,3,5,7,7,8,6,6,6,6};
+        int min=-1;
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+    }
+}
