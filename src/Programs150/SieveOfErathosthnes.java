aab341fbdeb229bf0e060dbf4a8aef1ed8f8e30e
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2022-12-30 16:18:25 +0530

    :octocat: :sparkles:

--- src/Programs150/SieveOfErathosthnes.java
+++ src/Programs150/SieveOfErathosthnes.java
@@ -3,20 +3,23 @@ package Programs150;
 public class SieveOfErathosthnes {
     public static void sieveOfEratosthenes(int n)
     {
-        boolean prime[] = new boolean[n + 1];
-        for (int i = 0; i <= n; i++)
-            prime[i] = true;
-
-        for (int p = 2; p <= n; p++) {
-            if (prime[p] == true) {
-                for (int i = p * p; i <= n; i += p)
-                    prime[i] = false;
+        boolean prime[] = new boolean[n+ 1];
+        for (int i=0;i<=n;i++)
+        {
+            prime[i]=true;
+        }
+        for (int p=2;p*p<n;p++){
+            if(prime[p]==true){
+                for (int i = p*p; i <=n ; i+=p) {
+                    prime[i]=false;
+                }
             }
         }
 
-        for (int i = 2; i <= n; i++) {
-            if (prime[i] == true)
-                System.out.print(i + " ");
+        for (int i = 2; i < n; i++) {
+            if(prime[i]==true){
+                System.out.println(i+" ");
+            }
         }
     }
 
