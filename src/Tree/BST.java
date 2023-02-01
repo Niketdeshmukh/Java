6e7667668d325a5e6d996afaff3fea627c3db1c0
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2023-02-01 15:15:49 +0530

    :octocat: :sparkles:

---
+++ src/Tree/BST.java
@@ -0,0 +1,45 @@
+package Tree;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class BST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+    public static Node insert(Node root,int val){
+        if(root==null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.data>val){
+            root.left = insert(root.left,val);
+        }
+        else{
+            root.right= insert(root.right,val);
+        }
+        return root;
+    }
+    public static void inorder(Node root) {
+        if(root== null) return ;
+        inorder(root.left);
+        System.out.println(root.data);
+        inorder(root.right);
+    }
+    public static void main(String args[]){
+        int[] values = {5,1,3,4,2,7};
+        Node root=null;
+        for (int value : values) {
+            root = insert(root, value);
+        }
+        inorder(root);
+        System.out.println();
+    }
+
+}
+
